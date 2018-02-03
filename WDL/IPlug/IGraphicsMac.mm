333f63e6cac7c7bcc0e01ace57b2997da3970e3f
Saverio Vigni <s.vigni@hor-net.com>
2018-02-03 15:28:58 +0100

    removed AH mod that prevente images from loading fine

--- WDL/IPlug/IGraphicsMac.mm
+++ WDL/IPlug/IGraphicsMac.mm
@@ -187,10 +187,8 @@ LICE_IBitmap* LoadImgFromResourceOSX(const char* bundleID, const char* filename)
   #endif
 
   NSBundle* pBundle = [NSBundle bundleWithIdentifier:ToNSString(bundleID)];
-  //NSString* pFile = [[[NSString stringWithCString:filename] lastPathComponent] stringByDeletingPathExtension];
-  //AH_CHANGE - I don't see why we can't use subfolders here?
-  NSString* pFile = [[NSString stringWithCString:filename] stringByDeletingPathExtension];
-  
+  NSString* pFile = [[[NSString stringWithCString:filename] lastPathComponent] stringByDeletingPathExtension];
+	
   if (pBundle && pFile)
   {
     NSString* pPath = 0;
